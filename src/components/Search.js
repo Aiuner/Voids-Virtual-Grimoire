bc8907f164487d5e349b79936ec1e44119ea0c03
Aiuner <demonspawndaemnyxea@gmail.com>
2020-08-04 15:50:09 -0400

    Fiddling around with Search Component.

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function Search() {
 
-  return null;
+
+
+  return (
+    <>
+      {/* <form onSubmit={handleSubmit}> */}
+      <form>
+        <input type="text" placeholder="Search Posts" />
+        <input type="submit" value="Search" />
+      </form>
+    </>
+  );
 }
 
 export default Search;
