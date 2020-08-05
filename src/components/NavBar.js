fbf66c76c1b6d24eed3f73da815abb79a3f159de
Aiuner <demonspawndaemnyxea@gmail.com>
2020-08-05 18:17:47 -0400

    Starting on Hamburger menu.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import Search from './Search'
 import '../assets/NavBar.css'
+import '../assets/hamburger-svg.png'
 
 function NavBar(props) {
   const [position, updatePosition] = useState('absolute');
@@ -20,10 +21,12 @@ function NavBar(props) {
   return (
     <>
       <nav onScroll={handleScroll} style={{position}} className={position === "absolute" ? "page-top" : "page-bottom"}>
-        <Search setSearchurl={props.setSearchurl}/>
-        <Link to="/">Home</Link>
-        <Link to="/posts/create_new_post">Create New Post</Link>
-        <Link to="/posts">All Posts</Link>
+        <div className="menu">
+          <Search setSearchurl={props.setSearchurl}/>
+          <Link to="/">Home</Link>
+          <Link to="/posts/create_new_post">Create New Post</Link>
+          <Link to="/posts">All Posts</Link>
+        </div>
       </nav>
     </>
   )
